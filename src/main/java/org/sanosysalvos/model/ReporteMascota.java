551d35ee0821a0faecb1f36c57e1ae18d77bb854
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-05-03 22:20:29 -0400

    modelo de datos JPA para el motor de coincidencias

---
+++ src/main/java/org/sanosysalvos/model/ReporteMascota.java
@@ -0,0 +1,39 @@
+package org.sanosysalvos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reporte_mascota")
+public class ReporteMascota {
+
+    @Id
+    @Column(name = "id_reporte_mascota")
+    private Long idReporteMascota;
+
+    @Column(name = "raza")
+    private String raza;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "tamano")
+    private String tamano;
+
+    @Column(name = "latitud")
+    private Double latitud;
+
+    @Column(name = "longitud")
+    private Double longitud;
+
+    @Column(name = "fecha_reporte")
+    private LocalDateTime fechaReporte;
+}
+
