88553fbfe2a17ff426ddda565a5e317302ab6868
Rodrigo Baeza <94202438+Electro-Kerubin@users.noreply.github.com>
2026-05-09 17:55:10 -0400

    feat: agregar findByIdTipoReporteNot para buscar reportes del tipo opuesto

--- src/main/java/org/sanosysalvos/repository/ReporteMascotaRepository.java
+++ src/main/java/org/sanosysalvos/repository/ReporteMascotaRepository.java
@@ -1,8 +1,15 @@
 package org.sanosysalvos.repository;
 
+import java.util.List;
 import org.sanosysalvos.model.ReporteMascota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReporteMascotaRepository extends JpaRepository<ReporteMascota, Long> {
+
+    /**
+     * Busca todos los reportes que NO sean del tipo indicado.
+     * Permite encontrar reportes del tipo opuesto (perdido vs encontrado).
+     */
+    List<ReporteMascota> findByIdTipoReporteNot(Integer idTipoReporte);
 }
 
